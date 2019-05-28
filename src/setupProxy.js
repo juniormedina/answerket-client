7fd1850937422038f98297e35db175d2cdd5f14b
Junior Medina <juniormedina.dev@gmail.com>
2019-05-28 21:22:32 +0800

    updated proxy for production

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,5 +2,5 @@ const proxy = require('http-proxy-middleware');
 
 module.exports = function(app) {
   // Development Proxy
-  app.use(proxy('/api/*', { target: 'http://localhost:5000/' }));
+  app.use(proxy('/api/*', { target: process.env.NODE_ENV === 'production' ? 'http://localhost:5000/' : 'https://answerket-server.herokuapp.com/' }));
 };
